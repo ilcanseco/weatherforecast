e01c7ba4db8d6f546797982d63cbfdfca193c84f
Ivan <ilcanseco97@gmail.com>
2019-05-03 01:17:18 -0400

    Helper function for converting the dayIndex to a day of the week.

--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,28 @@
 export const convertKelvinToCelcius = temp => {
   return Number.parseInt(temp - 273.15);
 };
+
+export const indexToDay = dayIndex => {
+  switch (dayIndex) {
+    case 1:
+      return "Mon";
+
+    case 2:
+      return "Tue";
+
+    case 3:
+      return "Wed";
+
+    case 4:
+      return "Thu";
+
+    case 5:
+      return "Fri";
+
+    case 6:
+      return "Sat";
+
+    default:
+      return "Sun";
+  }
+};
